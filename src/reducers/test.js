d60d80cb9f157067d3007554b083a7d37bd20a30
Szewczyk <mjsy@gft.com>
2018-05-14 13:41:12 +0200

    reducer and reducer's test

---
+++ src/reducers/test.js
@@ -0,0 +1,29 @@
+import { reducer, initialState } from '.';
+import types from '../constants/';
+
+describe('Reducer', () => {
+  const todoText = 'A todo';
+
+  it('should return the initial state when no action passed', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should return the correct state', () => {
+    const action = {
+      type: types.SUBMIT_TODO,
+      id: 1,
+      text: todoText
+    };
+
+    const expectedState = {
+      todos: [
+        {
+          id: 1,
+          text: todoText
+        }
+      ]
+    };
+
+    expect(reducer(undefined, action)).toEqual(expectedState);
+  });
+});
